e04063f9e4083e0a8bf1a9f86374c6a40e264cf9
Mirko Mälicke <mirko.maelicke@kit.edu>
2022-06-07 09:23:38 +0200

    update Home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,10 @@
-import { Alert, AppBar, Box, Fab, Toolbar, Typography } from "@mui/material";
+import { Alert, AppBar, Box, Fab, Toolbar, Typography, Zoom } from "@mui/material";
 import { ExpandMore } from '@mui/icons-material';
 
 import SxBox from "../components/SxBox";
 import { useRef } from "react";
 import AppPreview from "../components/AppPreview";
+import { InView } from "react-intersection-observer";
 
 const HomePage: React.FC = () => {
     // create scroll refs
@@ -21,6 +22,9 @@ const HomePage: React.FC = () => {
             <Box sx={{flexGrow: 1}}>
                 <AppBar position="sticky" color="default">
                     <Toolbar>
+                        <a href="https://hydrocode.de" target="_blank" rel="noreferrer">
+                        <img src="/logo.png" alt="logo" style={{height: 32, marginRight: '1.5rem'}} />
+                        </a>
                         <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
                             Geostatistics by hydrocode
                         </Typography>
@@ -31,8 +35,16 @@ const HomePage: React.FC = () => {
             <SxBox height="calc(100vh - 64px)" justifyContent="space-between">
                 <Box></Box>
                 <Box>
-                    <Typography variant="h1" component="div">Geostatistics</Typography>
-                    <Typography variant="h4" component="div">by hydrocode</Typography>
+                <Typography variant="h1" component="div">Geostatistics</Typography>
+                    <InView triggerOnce>
+                        {({inView, ref}) => (
+                            <Zoom ref={ref} in={inView} style={{transitionDelay: '500ms', transitionDuration: '450ms'}}>
+                                <Typography variant="h4" component="div">by hydrocode</Typography>
+                            </Zoom>
+                        )}
+                    </InView>
+                    
+                    
                 </Box>
                 <Box sx={{display: 'flex', justifyContent: 'center'}}>
                     <Fab color="primary" sx={{marginTop: '-64px'}} onClick={() => scrollHander(appRef)}>
@@ -55,14 +67,14 @@ const HomePage: React.FC = () => {
                 </SxBox>
 
                 <SxBox height="50vh">
-                <AppPreview imgSrc="https://www.fillmurray.com/g/300/300" reverse animate>
+                <AppPreview imgSrc="/img/uncertain_geostatistics.png" link="/uncertain" reverse animate>
                         <Typography variant="h3" component="div">Uncertain Geostatistics</Typography>
                         <Typography variant="body1" component="p" sx={{maxWidth: 500}}>
                             Start this chapter from SciKit-Uncertainty as a standalone application to
                             interactively explore how observation uncertainty influences each step of
                             geostistical workflows. 
                         </Typography>
-                        <Alert severity="warning">This application is currently maintained and not available.</Alert>
+                        <Alert severity="info">It is highly recommended to read the related paper. Link will follow.</Alert>
                     </AppPreview>
                 </SxBox>
             </div>
